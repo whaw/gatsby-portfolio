71f6a0cb1aa4021ca05f23f7f4c98d91620880c7
whaw <william.haw@gmail.com>
2022-11-18 10:19:34 -0500

    Resolving SSR hydration error, occuring in Gatsby react minifiation process

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
+import Layout from '../components/Layout'
 import '../scss/styles.scss'
 
-import Layout from '../components/Layout'
+const IndexPage = () => {
 
-const IndexPage = () => <Layout></Layout>
+  // adding domLoaded handler to address hydration error on server
+  // this is a SSR issue that occurs in the Gatsby minification process
+  // creating the site - running code dependant on the dom - before dom is loaded
+  const [domLoaded, setDomLoaded] = useState(false)
+
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
+
+  return (
+    <>
+      {domLoaded && <Layout></Layout>}
+    </>
+  )
+}
 
 export default IndexPage
 
