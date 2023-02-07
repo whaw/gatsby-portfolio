3f0a59f1fc62a19545285e5d1bec73082187deed
whaw <william.haw@gmail.com>
2023-02-07 10:05:46 -0500

    Addressing jquery sizzle error with smooth scroll code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,5 @@
 import 'jquery/dist/jquery.slim.min.js'
 import '@popperjs/core/dist/umd/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import $ from "jquery"
 
-export const onInitialClientRender = () => {
-  $(function () {
-    // add smooth scroll to links
-    // (css option is too fast in safari)
-    $("a")
-      .on("click", function () {
-        const id = $(this).attr("href")
-        const offsetTop = $(id).offset().top
-        $("html, body").animate({ scrollTop: offsetTop }, "slow")
-      })
-  })
-}
+// export const onInitialClientRender = () => {}
