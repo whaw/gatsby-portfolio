dd8d527ad77d2f7bbbfdcd103bf05a3cbf537a79
whaw <william.haw@gmail.com>
2023-03-06 16:56:30 -0500

    Refactoring

--- src/components/Hero/Tools/Tools.js
+++ src/components/Hero/Tools/Tools.js
@@ -2,17 +2,17 @@ import React from "react"
 import Tool from "./Tool"
 
 const Tools = ({ toolAnimDetails }) => {
-
-  const toolIcons = toolAnimDetails[0].images.map(({ selector, name, description, status, image }) =>
-    <Tool
-      selector={selector}
-      name={name}
-      description={description}
-      status={status}
-      key={image}
-    />)
-
-  return toolIcons
+  return (
+    toolAnimDetails[0].images.map(({ selector, name, description, status, image }) =>
+      <Tool
+        selector={selector}
+        name={name}
+        description={description}
+        status={status}
+        key={image}
+      />
+    )
+  )
 }
 
 export default Tools
