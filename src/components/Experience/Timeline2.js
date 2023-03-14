e199abf586320557ff050b3beee92c6592e017a2
whaw <william.haw@gmail.com>
2023-03-14 12:01:35 -0400

    Addressing timeline resize bug, from Section component refactor

--- src/components/Experience/Timeline2.js
+++ src/components/Experience/Timeline2.js
@@ -6,7 +6,7 @@ const Timeline2 = () => {
     experienceJson.map(({ company, period, copy }, i) => {
       const colPosClass = i % 2 === 0 ? 'left' : 'right'
       return (
-        <div key={company} className={`container position-relative gs_reveal ${colPosClass}`}>
+        <div key={company} className={`timeline__container position-relative gs_reveal ${colPosClass}`}>
           <div className="content">
             <h5>{company}</h5>
             <p className="text-secondary">{period}</p>
