3748845c01c394243c29edf9a0b04567d94f5cbe
whaw <william.haw@gmail.com>
2023-03-15 10:00:00 -0400

    Continuing fix for timeline gsap bug

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -4,7 +4,13 @@ import Section from "../Section"
 
 const Experience = () => (
   <Section sectionId="experience" header="Experience" subHeader="At a glance" sectionClass="bg-light section--with-background">
-    <Timeline2 />
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <Timeline2 />
+        </div>
+      </div>
+    </div>
   </Section>
 )
 
