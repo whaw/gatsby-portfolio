1411b6f842e1ee4bbab827d7303edc6b458975e4
whaw <william.haw@gmail.com>
2023-03-15 11:11:55 -0400

    Adding fix for tool description headers accessibility

--- src/components/Hero/Tools/ToolDescription.js
+++ src/components/Hero/Tools/ToolDescription.js
@@ -6,7 +6,7 @@ const ToolDescription = ({ name, description, status, isOpen }) => {
       className={`hero__tool__aside border bg-white text-left small mt-2 ${isOpen ? "animate pt-3 px-2" : ""
         }`}
     >
-      <h6>{name}</h6>
+      <div role="heading" aria-level="6" className="h6">{name}</div>
       <p>{description}</p>
       <p>
         <span className="font-weight-bold">Status:</span>{" "}
