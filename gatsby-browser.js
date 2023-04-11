e09d27889914accefe7bcf3b46f28167c8b1cf93
whaw <william.haw@gmail.com>
2023-04-11 10:52:59 -0400

    Adding hamburger nav fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
-// export const onInitialClientRender = () => {}
+import 'jquery/dist/jquery.slim.min.js'
+import '@popperjs/core/dist/umd/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+/* export const onInitialClientRender = () => {} */
+
+
