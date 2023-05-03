cc337287a125a78b29c90cb261346dd6d615c81d
whaw <william.haw@gmail.com>
2023-05-03 19:50:40 -0400

    Updating text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,11 @@ import SectionHeader from "./layout/SectionHeader"
 
 const Contact = () => (
   <section id="contact">
-    <SectionHeader header="Give me a&nbsp;shout" subHeader="I'm up for anything!" />
+    <SectionHeader header="Give me a&nbsp;shout" subHeader="Looking for great teams" />
     <div className="container">
       <div className="row">
         <div className="col-12 pt-md-10 pb-5 pb-md-10 text-center gs_reveal">
-          <p className="small text-secondary">Contact information has been provided as part of application&nbsp;submissions. Sorry spammers!</p>
+          <p className="small text-secondary">Contact information has been submitted with applications. Sorry spammers!</p>
         </div>
       </div>
     </div>
