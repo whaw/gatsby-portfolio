0d5bd94e3dd45cceaaff94f2f214b1e3b511da36
whaw <william.haw@gmail.com>
2023-05-03 19:51:36 -0400

    Tweaking stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,28 +6,28 @@ const Stats = () => {
       <div className="row">
         <div className="col-3 text-center gs_reveal">
           <div className="text-left mx-auto d-lg-inline-block">
-            <div className="stat text-muted d-inline">15</div>&nbsp;
+            <div className="display-2 stat text-secondary d-inline">15</div>&nbsp;
             <div className="stat_unit d-inline">YRS</div><br />
             <small className="pl-2">Front-end development</small>
           </div>
         </div>
         <div className="col-3 text-center gs_reveal">
           <div className="text-left mx-auto d-lg-inline-block">
-            <div className="stat text-muted d-inline">11</div>&nbsp;
+            <div className="display-2 stat text-secondary d-inline">11</div>&nbsp;
             <div className="d-inline">YRS</div><br />
             <small className="pl-2">Agency experience</small>
           </div>
         </div>
         <div className="col-3 text-center gs_reveal">
           <div className="text-left mx-auto d-lg-inline-block">
-            <div className="stat text-muted d-inline">7</div>&nbsp;
+            <div className="display-2 stat text-secondary d-inline">7</div>&nbsp;
             <div className="d-inline">YRS</div><br />
             <small className="pl-2">Email development</small>
           </div>
         </div>
         <div className="col-3 text-center gs_reveal">
           <div className="text-left mx-auto d-lg-inline-block">
-            <div className="stat text-muted d-inline">8</div>&nbsp;
+            <div className="display-2 stat text-secondary d-inline">8</div>&nbsp;
             <div className="d-inline">YRS</div><br />
             <small className="pl-2">CMS development</small>
           </div>
