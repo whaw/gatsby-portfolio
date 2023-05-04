8968a07f2c96e2779339264cdcd02cab4ee4f108
whaw <william.haw@gmail.com>
2023-05-04 14:38:45 -0400

    Refactoring

--- src/components/MainNav/BrandButton.js
+++ src/components/MainNav/BrandButton.js
@@ -6,7 +6,7 @@ const BrandButton = () => {
     <button className="navbar-brand js_logo pb-2 border-0 bg-white text-left p-0" onClick={() => siteJsUtils.scrollHome()}>
       <span className="logo mb-2 d-block">A working copy</span>
       <span className="d-none d-md-inline-block">William Haw &mdash; Front-end developer
-        <span className="d-none d-xl-inline-block">, Outdoor enthusiast</span>
+        <span className="d-none d-xl-inline-block">, outdoor enthusiast</span>
       </span>
     </button>
   )
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,37 +1,24 @@
 import React from 'react'
 
 const Stats = () => {
+  const statsJson = [
+    { id: 1, value: 15, unit: "YRS", text: "Front-end development" },
+    { id: 2, value: 11, unit: "YRS", text: "Agency experience" },
+    { id: 3, value: 7, unit: "YRS", text: "Email development" },
+    { id: 4, value: 8, unit: "YRS", text: "CMS development" }
+  ]
   return (
-    <div className="stats container d-none d-lg-block">
+    <div className="stats container d-none d-lg-block gs_reveal">
       <div className="row">
-        <div className="col-3 text-center gs_reveal">
-          <div className="text-left mx-auto d-lg-inline-block">
-            <div className="display-2 stat text-secondary d-inline">15</div>&nbsp;
-            <div className="stat_unit d-inline">YRS</div><br />
-            <small className="pl-2">Front-end development</small>
+        {statsJson.map(({ id, value, unit, text }) =>
+          <div key={id} className="col-3 text-center">
+            <div className="text-left mx-auto d-lg-inline-block">
+              <div className="display-2 stat text-secondary d-inline">{value}</div>&nbsp;
+              <div className="stat_unit d-inline">{unit}</div><br />
+              <small className="pl-2">{text}</small>
+            </div>
           </div>
-        </div>
-        <div className="col-3 text-center gs_reveal">
-          <div className="text-left mx-auto d-lg-inline-block">
-            <div className="display-2 stat text-secondary d-inline">11</div>&nbsp;
-            <div className="d-inline">YRS</div><br />
-            <small className="pl-2">Agency experience</small>
-          </div>
-        </div>
-        <div className="col-3 text-center gs_reveal">
-          <div className="text-left mx-auto d-lg-inline-block">
-            <div className="display-2 stat text-secondary d-inline">7</div>&nbsp;
-            <div className="d-inline">YRS</div><br />
-            <small className="pl-2">Email development</small>
-          </div>
-        </div>
-        <div className="col-3 text-center gs_reveal">
-          <div className="text-left mx-auto d-lg-inline-block">
-            <div className="display-2 stat text-secondary d-inline">8</div>&nbsp;
-            <div className="d-inline">YRS</div><br />
-            <small className="pl-2">CMS development</small>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   )
