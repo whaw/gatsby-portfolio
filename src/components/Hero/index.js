9afafbaf977f92833bc83811757b8a8043c7b3c8
William Haw <whaw@blackjet.ca>
2025-01-07 14:28:01 -0500

    Tweaking down arrow button

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -31,7 +31,7 @@ const Hero = () => {
     <section className="hero position-relative js_hero mt-5 mt-md-7 pt-0">
       <div className="hero__heading js_heading position-absolute text-center w-100 pt-2 pt-sm-2 pt-md-3 pt-lg-4 pt-xl-6">
         <h1 className="hero__heading__h1 mb-0 pt-sm-3 pt-md-0 gs_reveal">Be <span>responsive</span></h1>
-        <a href="#introductions" role="button" aria-label="down button" className="hero__down-button js_hero_button mt-5 gs_reveal">&#x2304;</a>
+        <a href="#introductions" role="button" aria-label="down button" className="hero__down-button text-decoration-none js_hero_button mt-5 gs_reveal">&#x2304;</a>
       </div>
       <Tools toolAnimDetails={toolAnimDetails} />
       <div className="mx-auto position-absolute hero__hashTag text-white text-center d-none d-sm-block">
