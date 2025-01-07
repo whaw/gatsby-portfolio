fe9d4cb25f28695fcd3bb3632d3bb017a9d5d773
William Haw <whaw@blackjet.ca>
2025-01-07 12:41:29 -0500

    Updating copy

--- src/components/AboutSite.js
+++ src/components/AboutSite.js
@@ -9,27 +9,33 @@ const AboutSite = () => (
       <div className="row">
         <div className="col-12 col-md-6 pr-lg-10 gs_reveal">
           <div role="heading" aria-level="5" className="h5">Situation</div>
-          <p className="mb-0"><strong>This site serves a few purposes:</strong></p>
+          <p>Being able to learn and deliver on new tech is essential in any role in my experience, so I thought it beneficial to show how that looks – best viewed through that lens.</p>
+          <p className="mb-0"><strong>So this site serves a few purposes:</strong></p>
           <ol>
-            <li>to provide a sample of execution and delivery,</li>
-            <li>to act as a sandbox for experimentation and discovery, and</li>
+            <li>to provide an example of work when adopting new technology</li>
+            <li>to demonstrate a level of polish in delivery,</li>
+            <li>to act as a sandbox for experimentation and discovery to learn and grow,</li>
             <li>to provide a little about me (fit being important).</li>
           </ol>
           <div role="heading" aria-level="5" className="h5">Task</div>
-          <p>To develop a Gatsby, React website with an emphasis on growing proficiency in JavaScript tooling, and to demonstrate a level of polish and experience in delivery, based on web standards and best practices.</p>
+          <p>To develop a React website with an emphasis on growing proficiency in JavaScript tooling.</p>
         </div>
         <div className="col-12 col-md-6 gs_reveal">
           <div role="heading" aria-level="5" className="h5">Approach</div>
           <ul>
-            <li>Gatsby, optimized for performance out of the box (thanks to static rendering)</li>
-            <li>Bootstrap for faster development, with a cleaner, more maintainable code base</li>
+            <li>Gatsby for performance, and for being built on React</li>
+            <li>Bootstrap for faster development, less custom code and (some) future-proofing</li>
             <li>Animations are with CSS, JavaScript and GSAP</li>
             <li>Other areas of focus: React best practices and design patterns</li>
-            <li>Prettier and ESLint to keep everything clean and error free.</li>
+            <li>Prettier and ESLint to keep everything clean and error-free</li>
           </ul>
-          <div role="heading" aria-level="5" className="h5">Result</div>
-          <p>Site is fast, consistent, accessible and performant.</p>
-          <p>Increased knowledge in Gatsby, React, JavaScript, GraphQL and GSAP.</p>
+          <div role="heading" aria-level="5" className="h5">Results</div>
+          <ul>
+            <li>Site is fast, consistent, accessible and performant</li>
+            <li>Increased knowledge in Gatsby, React, JavaScript, GraphQL and GSAP</li>
+          </ul>
+
+          <p><strong>Next:</strong> TypeScript, Jest, React Testing library and Next.js</p>
           <p className="text-center text-md-left">
             <Button
             onClick={() => window.open('https://github.com/whaw/whaw.github.io/tree/source?utm_source=portsite&utm_medium=about__to_code_link')}
