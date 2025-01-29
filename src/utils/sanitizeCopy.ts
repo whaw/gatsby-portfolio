1b50573ac92cf14c4f698a73b6ff6cafe8c515c5
William Haw <william.haw@gmail.com>
2025-01-29 15:10:30 -0500

    Tweaking

--- src/utils/sanitizeCopy.ts
+++ src/utils/sanitizeCopy.ts
@@ -1,7 +1,7 @@
 import DOMPurify from "dompurify";
 
 const sanitizeCopy = (copy: string) => {
-  return { __html: DOMPurify.sanitize(copy) };
+  return { __html: DOMPurify.sanitize(copy || "") };
 }
 
 export default sanitizeCopy;
